99ea65dc6e9c32a45eb904cd2215913687226e36
Darksuhh <123446177+Darksuhh@users.noreply.github.com>
2023-11-01 14:46:14 +0200

    Revert "fixed food.cs"
    This reverts commit 03bd680ff00b5af0b279692be3a0ffd193471aa6.

--- Assets/Food.cs
+++ Assets/Food.cs
@@ -15,7 +15,7 @@ public class Food : MonoBehaviour
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
 
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+        this.transform = Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
